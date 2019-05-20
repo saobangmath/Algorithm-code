5a32e38810304f2f136ef44d0992952fd5d27a26
saobangmath <42882997+saobangmath@users.noreply.github.com>
2019-05-20 14:04:45 +0700

    Add files via upload
    Codes to generate all the matrix 4*4 that has each row and column average lies on such row and column 
    with the number in the range(0,16)

---
+++ matrix_check.py
@@ -0,0 +1,55 @@
+def check(r):
+    for i in r:
+        if (i==-1):
+            return False
+    return True
+
+def check_valid(b):
+    for e in b:
+        if check(e) and (sum(e)/4) not in e:
+            return False
+    for i in range(4):
+        c = [e[i] for e in b]
+        if check(c) and (sum(c)/4) not in c:
+            return False
+    return True
+
+def show(board):
+    for i in range(4):
+        for j in range(4):
+            print(board[i][j],end=" ")
+        print("\n")
+
+    print("##########")
+board=[[-1 for i in range(4)] for j in range(4)]
+mark=[False for i in range(16)]
+res=[]
+def backtrack(board,r,c):
+    if (r==4):
+        if (board not in res):
+            show(board)
+            return
+
+    else:
+        if (c == 3):
+            for num in range(16):
+                if(not mark[num]):
+                    board[r][c] = num
+                    if (check_valid(board)):
+                        mark[num] = True
+                        backtrack(board,r+1,0)
+                        mark[num] = False
+                    board[r][c]=-1
+
+        else:
+            for num in range(16):
+                if(not mark[num]):
+                    board[r][c] = num
+                    if (check_valid(board)):
+                        mark[num] = True
+                        backtrack(board,r,c+1)
+                        mark[num] = False
+                    board[r][c]=-1
+
+backtrack(board,0,0)
+
